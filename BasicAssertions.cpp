f4620ceb268d0c447cbaa0d0c562cfef9cf2d16b
かおるん <kaorun55@gmail.com>
2010-07-29 02:18:26 +0900

    基本的なアサーションを追加

---
+++ BasicAssertions.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+// EXPECT_TRUE
+TEST( Basic, EXPECT_TRUE )
+{
+  EXPECT_TRUE( true );  // 結果がtrueなら成功
+  EXPECT_TRUE( false ); // 結果がfalseなら失敗
+}
+
+// EXPECT_FALSE
+TEST( Basic, EXPECT_FALSE )
+{
+  EXPECT_FALSE( false ); // 結果がfalseなら成功
+  EXPECT_FALSE( true );  // 結果がtrueなら失敗
+}
--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@ test: AssertAndExpect
 AssertAndExpect: AssertAndExpect.cpp
 	g++ AssertAndExpect.cpp -lgtest -lgtest_main
 	./a.out
+
+BasicAssertions: BasicAssertions.cpp
+	g++ BasicAssertions.cpp -lgtest -lgtest_main
+	./a.out
