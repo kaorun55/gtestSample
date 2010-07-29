8b8ad7f4a31ffd8358cb3cc53953c7a0210999e5
かおるん <kaorun55@gmail.com>
2010-07-29 23:14:49 +0900

    EXPECT_STRNE,EXPECT_STRCASEEQ,EXPECT_STRCASENEを作成

--- StringComparison.cpp
+++ StringComparison.cpp
@@ -3,6 +3,27 @@
 TEST( StringComparison, EXPECT_STREQ )
 {
   EXPECT_STREQ( "abcd", "abcd" );  // 同じ文字列であれば成功
-  EXPECT_STREQ( "abcd", "ABCD" );  // 違う文字列であれば失敗(大文字、小文字も比較)
+  EXPECT_STREQ( "abcd", "bcde" );  // 違う文字列であれば失敗
+  EXPECT_STREQ( "abcd", "ABCD" );  // 同じ文字列でも、大文字、小文字の違いは失敗
+}
 
+TEST( StringComparison, EXPECT_STRNE )
+{
+  EXPECT_STRNE( "abcd", "bcde" );  // 違う文字列であれば成功
+  EXPECT_STRNE( "abcd", "ABCD" );  // 同じ文字列でも、大文字、小文字の違いは成功
+  EXPECT_STRNE( "abcd", "abcd" );  // 同じ文字列であれば失敗
+}
+
+TEST( StringComparison, EXPECT_STRCASEEQ )
+{
+  EXPECT_STRCASEEQ( "abcd", "abcd" );  // 同じ文字列であれば成功
+  EXPECT_STRCASEEQ( "abcd", "bcde" );  // 違う文字列であれば失敗
+  EXPECT_STRCASEEQ( "abcd", "ABCD" );  // 同じ文字列で、大文字、小文字の違いは成功
+}
+
+TEST( StringComparison, EXPECT_STRCAENE )
+{
+  EXPECT_STRCASENE( "abcd", "bcde" );  // 違う文字列であれば成功
+  EXPECT_STRCASENE( "abcd", "ABCD" );  // 同じ文字列で、大文字、小文字の違いは失敗
+  EXPECT_STRCASENE( "abcd", "abcd" );  // 同じ文字列であれば失敗
 }
